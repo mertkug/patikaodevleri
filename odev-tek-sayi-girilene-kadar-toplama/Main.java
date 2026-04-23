00fd071e5f35b0b9008723a3ceec01ba7a9a2079
Mert KUĞ <56736947+mertkug@users.noreply.github.com>
2026-04-23 16:53:32 +0300

    Tek sayiya kadar toplam programi

---
+++ odev-tek-sayi-girilene-kadar-toplama/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sayi, toplam = 0;
+
+        do {
+            System.out.print("Sayi giriniz: ");
+            sayi = input.nextInt();
+
+            if (sayi % 2 == 0 && sayi % 4 == 0) {
+                toplam += sayi;
+            }
+        } while (sayi % 2 == 0);
+
+        System.out.println("Toplam: " + toplam);
+
+        input.close();
+    }
+}
